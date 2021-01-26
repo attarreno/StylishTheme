626bc5f6946c588ab99c6bfaed71dc417768a478
attarreno <67298388+attarreno@users.noreply.github.com>
2021-01-26 15:28:58 +0700

    Add files via upload

---
+++ Stylish.sh
@@ -0,0 +1,82 @@
+#/bin/bash
+
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+toilet -f mini -F gay Stylish
+echo Script By : Attar Nation / Attar Fvnky | lolcat
+sleep 1.0
+
+echo My Hacker Name : Cyborg Project | lolcat -a -d 20
+echo $green"=================================="
+echo Author Script Stylish : Attar Fvnky | lolcat
+echo $yellow"Subscribe Yt || Attar Nation "
+echo $cyan"Follow Ig : @attarnation007"
+echo $green"=================================="
+echo $green"----------------------------------"
+echo $purple"[0].Exits Script"
+echo $red"[1].Anonymously 's Main theme termux"
+echo $white"[2].White dragons Termux Theme"
+echo $cyan"[3].Anonymous V2 Tool"
+echo $green"----------------------------------"
+echo $yellow"Tracker IP Tools V1 Script Tools"
+echo $green"----------------------------------"
+echo $yellow"[OIP].Own Ip Tracer"
+echo $yellow"[IP].IP SubDomain"
+echo $green"----------------------------------"
+read -p "INPUT THE NUMBER ABOVE :" Select
+if [ $Select = 1 ] || [ $Select = 1 ]
+then
+clear
+cowsay -f eyes "Welcome To Termux" | lolcat
+toilet -f mini "AttarNation" -F gay
+neofetch
+date | lolcat
+fi
+
+if [ $Select = 2 ] || [ $Select = 2 ]
+then
+clear
+cowsay -f dragon "Dragon theme"
+toilet -f mini "AttarNation" -F gay
+neofetch
+cal | lolcat
+date | lolcat
+fi
+
+if [ $Select = 0 ] || [ $Select = 0 ]
+then
+echo Exit The script | lolcat
+clear
+exit
+fi
+
+if [ $Select = OIP ] || [ $Select = OIP ]
+then
+sleep 1.5
+echo Warning :Click CTRL + C To Stop | lolcat
+netstat - an
+sh Stylish.sh
+fi
+
+if [ $Select = IP ] || [ $Select = IP ]
+then
+clear
+echo $red"Warning : Click CTRL + C  To stop"
+sleep 1.5
+echo $green"Notice : Input the Sub domain"
+nslookup
+fi
+
+if [ $Select = 3 ] || [ $Select = 3 ]
+then
+cowsay -f eyes Anonymous Theme | lolcat
+echo Anonymous Themes | lolcat -a -d 01
+cal | lolcat
+date | lolcat
+fi
